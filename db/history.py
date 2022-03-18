38d0ad2b1e573975e8c5e8322d401881f1003cb0
Shripad Rao <shripada0987@gmail.com>
2022-03-18 14:47:23 +0530

    inital commit

---
+++ db/history.py
@@ -0,0 +1,18 @@
+#print sql data here.
+import sqlite3 as sql
+import os
+import csv
+
+path = "/home/shripad/Projects/AI-Pose-Estimation/Rep Counter/db/rep.db"
+conn =  sql.connect(path)
+
+print("\n *--History--*")
+print("start_time, end_time, date, left, right, total")
+cur = conn.cursor()
+cur.execute('''SELECT * FROM rep_counter''')
+rows = cur.fetchall()
+   
+for row in rows:
+    print(row)
+
+conn.close()
